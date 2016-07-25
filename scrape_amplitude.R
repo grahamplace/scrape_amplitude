35a60ae9cdb4c36a0c8d6b427af4bc105142cc80
Maggie Lou <margaretlou2019@u.northwestern.edu>
2016-07-25 17:28:41 +0200

    Initial Scraping code.

--- scrape_amplitude.R
+++ scrape_amplitude.R
@@ -1,36 +1,32 @@
-URL <- "https://amplitude.com/app/146509/activity/search?userId=5547129680"
-
-library(XML)
-df <- readHTML(URL, which = 1, stringsAsFactors = FALSE)
-
 library(rvest)
-amplitude <- read_html(URL)
-test <- amplitude %>% html_nodes("#main > div > div.main-content.ng-scope > div > div.ng-scope > div:nth-child(1) > div.loading-wrapper.ng-isolate-scope > div.loading-window > div > div:nth-child(1) > table:nth-child(1) > tbody > tr:nth-child(2) > td.ellipsis-overflow.ng-binding")
-
 library(RSelenium)
+
+#open browser 
 checkForServer()
 startServer()
-browser <- remoteDriver(browserName = "chrome")
+browser <- remoteDriver()
 browser$open()
-browser$navigate("http://www.google.com")
-browser$getCurrentUrl()
-browser$getTitle()
-# Locate the input field using a CSS selector.
-input <- browser$findElement(using = 'css selector', "#lst-ib")
-input$sendKeysToElement(list("CRAN Selenium"))
+url <- "https://amplitude.com/login"
+browser$navigate(url)
+
+#locate search bars
+email_input <- browser$findElement(using = 'css selector', '#login-email')
+email_input$sendKeysToElement(list("margaretlou2019@u.northwestern.edu"))
+password_input <- browser$findElement(using = 'css selector', '#login-password')
+password_input$sendKeysToElement(list("maggie_lou"))
+button <- browser$findElement(using = 'xpath', '/html/body/div[2]/div/form/fieldset/div[3]/button')
+button$clickElement()
+
+browser$navigate("https://amplitude.com/app/146509/activity/search?userId=8408666528")
+table <- browser$findElement(using = 'css selector', 'table.table.half.user-details-table')
+table <- readHTMLTable(header = TRUE, table$getElementAttribute("outerHTML")[[1]])
 
 
-library(httr)
-library(XML)
 
-handle <- handle("https://amplitude.com/login") 
-path   <- "amember/login.php"
 
-# fields found in the login form.
-login <- list(
-  amember_login = "grahamgplace@gmail.com"
-  ,amember_pass  = "leopardskloof"
-  ,amember_redirect_url = "https://amplitude.com/app/146509/activity/search?userId=5508109519"
-)
-
-response <- POST(handle = handle, path = path, body = login)
+#login-email simplified
+remDr$open()
+remDr$navigate(appURL)
+remDr$findElement("id", "login")$sendKeysToElement(list("myusername"))
+remDr$findElement("id", "pass")$sendKeysToElement(list("mypass"))
+remDr$findElement("css", ".am-login-form input[type='submit']")$clickElement()
