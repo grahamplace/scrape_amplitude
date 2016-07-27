5f471698c257f693bd3ebefb14c5e004a2046fc0
Graham Place <grahamgplace@gmail.com>
2016-07-27 14:52:12 +0200

    Add files via upload
    add general scrape script

---
+++ automate_scraping_historical.R
@@ -0,0 +1,47 @@
+
+update_users <- function(x) {
+  
+  #connect to db
+  library(RSQLite)
+  db <- dbConnect(SQLite(), "overDB.sqlite")
+  
+  #check validity before making moves
+  if(dbIsValid(db)){
+    
+    #load master file of 30 day user set
+    users_master <- read.csv("users_master.csv")
+    
+    #check that users table exists in db
+    if(dbExistsTable("users")) {
+      
+      #load table of already scraped users
+      users_scraped <- dbReadTable(db, "users")
+      
+      #get chunk of master user cohort that hasn't been scraped already
+      to_scrape <- users_master[-which(users_master[,1] %in% users_scraped[,1]),]
+      
+      #scrape users that aren't yet in database
+      scrape_amplitude(to_scrape)
+      
+      #scrape_amplitude continually overwrites csv file. read that csv back in after function crashes
+      temp_scraped <- read.csv("temp_scraped.csv")
+      
+      #append everything just scraped to users table in db
+      dbWriteTable(db, "users", temp_scraped, append = TRUE)
+      
+      #disconnect db to ensure proper writing of users table 
+      dbDisconnect(db)
+      
+      #success
+      return (TRUE)
+    }
+    else {
+      return (FALSE)
+    }
+  }
+  
+  else {
+    return (FALSE)
+  }
+}
+
