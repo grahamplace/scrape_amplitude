a135f0d75de25a0ede2a4237e18ddc4df601fb44
mlou19 <margaretlou2019@u.northwestern.edu>
2016-07-28 16:57:41 +0200

    Add error codes.

--- automate_scraping_historical.R
+++ automate_scraping_historical.R
@@ -1,5 +1,5 @@
 
-update_users <- function(x) {
+update_users <- function() {
   
   #connect to db
   library(RSQLite)
@@ -12,10 +12,11 @@ update_users <- function(x) {
     users_master <- read.csv("users_master.csv")
     
     #check that users table exists in db
-    if(dbExistsTable("users")) {
+    if(dbExistsTable(db,"users")) {
       
       #load table of already scraped users
       users_scraped <- dbReadTable(db, "users")
+      users_scraped <- users_scraped[,-1]
       
       #get chunk of master user cohort that hasn't been scraped already
       to_scrape <- users_master[-which(users_master[,1] %in% users_scraped[,1]),]
@@ -25,6 +26,7 @@ update_users <- function(x) {
       
       #scrape_amplitude continually overwrites csv file. read that csv back in after function crashes
       temp_scraped <- read.csv("temp_scraped.csv")
+      temp_scraped<- temp_scraped[,-1]
       
       #append everything just scraped to users table in db
       dbWriteTable(db, "users", temp_scraped, append = TRUE)
@@ -36,11 +38,13 @@ update_users <- function(x) {
       return (TRUE)
     }
     else {
+      print("Users table does not exist.")
       return (FALSE)
     }
   }
   
   else {
+    print("Database is not valid.")
     return (FALSE)
   }
 }
